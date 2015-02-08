fed1877f7a906337662ada28f56ffc685bc9d90f
itsrainingdata <Zigmund-2@Nicks-MacBook-Pro.local>
2015-02-07 17:47:54 -0800

    Added random.sparse

--- tests/testthat/helper-random_dag.R
+++ tests/testthat/helper-random_dag.R
@@ -9,3 +9,24 @@ random.dag.matrix <- function(pp, nedge){
 
     m
 }
+
+### Generate a random sparse matrix with <= s nonzero elements (returned as a matrix object)
+random.sparse <- function(dim, s, diag = TRUE){
+    if(length(dim) == 1){
+        nrow <- ncol <- dim
+    } else if(length(dim) == 2){
+        nrow <- dim[1]
+        ncol <- dim[2]
+    } else{
+        stop("dim must have either one or two components!")
+    }
+
+    m <- matrix(0, nrow = nrow, ncol = ncol)
+    for(i in 1:s){
+        m[sample(1:nrow, 1), sample(1:ncol, 1)] <- rnorm(1)
+    }
+
+    if(!diag) diag(m) <- rep(0, ncol)
+
+    m
+}
