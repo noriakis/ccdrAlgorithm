71600ca1c94dc9437be3ca59add1aa28392f08d3
itsrainingdata <Zigmund-2@Nicks-MacBook-Pro.local>
2015-02-07 18:05:58 -0800

    Updated as.sparse tests to be more comprehensive

--- tests/testthat/test-as_sparse.R
+++ tests/testthat/test-as_sparse.R
@@ -1,9 +1,37 @@
-# context("as.sparse")
-#
-# test_that("as.sparse converts matrix objects correctly", {
-#     m <- random.dag.matrix(10, 10)
-#     sp <- as.sparse(m)
-#
-#     expect_equal(as.matrix(sp), m)
-#     # sum(abs(m-as.matrix(sp)))
-# })
+context("sparse to matrix conversion")
+
+test_that("as.matrix -> as.sparse -> as.matrix makes no changes", {
+
+    ### Test the zero matrix
+    m <- matrix(rep(0, 1), ncol = 1)
+    expect_equal(as.matrix(as.sparse(as.matrix(m))), m)
+    m <- matrix(rep(0, 4), ncol = 2)
+    expect_equal(as.matrix(as.sparse(as.matrix(m))), m)
+    m <- matrix(rep(0, 12), ncol = 3) # non-square case
+    expect_equal(as.matrix(as.sparse(as.matrix(m))), m)
+
+    ### Test a randomly generated matrix
+    m <- random.sparse(5, 5)
+    expect_equal(as.matrix(as.sparse(as.matrix(m))), m)
+
+    ### Test a randomly generated DAG
+    m <- random.dag.matrix(10, 10)
+    expect_equal(as.matrix(as.sparse(as.matrix(m))), m)
+})
+
+test_that("as.matrix -> as.sparse -> as.matrix makes no changes to REALLY BIG matrices", {
+
+    ### Test the zero matrix
+    m <- matrix(rep(0, 100*100), ncol = 100)
+    expect_equal(as.matrix(as.sparse(as.matrix(m))), m)
+
+    ### Test a randomly generated matrix
+    m <- random.sparse(100, 5*100)
+    expect_equal(as.matrix(as.sparse(as.matrix(m))), m)
+
+    ### Test a randomly generated DAG
+    m <- random.dag.matrix(100, 5*100)
+    expect_equal(as.matrix(as.sparse(as.matrix(m))), m)
+})
+
+test_that("as.sparse converts matrix objects correctly", {})
