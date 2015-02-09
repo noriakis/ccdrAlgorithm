c57fb6e2d6af33c1c665d355f3dcfc4db5017ffd
itsrainingdata <Zigmund-2@Nicks-MacBook-Pro.local>
2015-02-08 19:30:34 -0800

    Finished type-checking tests for .ccdr.singleR

--- tests/testthat/test-ccdr_singleR.R
+++ tests/testthat/test-ccdr_singleR.R
@@ -6,10 +6,12 @@ cors.length <- pp*(pp+1)/2
 cors.test <- runif(cors.length)
 betas.test <- matrix(runif(pp*pp), ncol = pp)
 
-test_that("Check input: cors", {
-
-    ### No error
+test_that(".ccdr.singleR runs as expected", {
+    ### No error / Also tests default values for betas, gamma, eps, maxIters, alpha
     expect_error(not(.ccdr.singleR(cors = cors.test, pp = pp, nn = nn, lambda = pi)))
+})
+
+test_that("Check input: cors", {
 
     ### Throw error if cors is integer
     expect_error(.ccdr.singleR(cors = as.integer(cors.test), pp = pp, nn = nn, lambda = pi))
@@ -43,18 +45,46 @@ test_that("Check input: betas", {
     expect_true(is.zero(final$sbm))
 })
 
-test_that("Check input: ", {
+test_that("Check input: gamma", {
+    ### gamma is non-numeric
+    expect_error(.ccdr.singleR(cors = cors.test, pp = pp, nn = nn, lambda = pi, gamma = 1L))
+
+    ### gamma is negative
+    expect_error(.ccdr.singleR(cors = cors.test, pp = pp, nn = nn, lambda = pi, gamma = -5))
+
+    ### gamma = -1 is OK
+    expect_error(not(.ccdr.singleR(cors = cors.test, pp = pp, nn = nn, lambda = pi, gamma = -1)))
 
 })
-test_that("Check input: ", {
 
+test_that("Check input: eps", {
+    ### eps is non-numeric
+    expect_error(.ccdr.singleR(cors = cors.test, pp = pp, nn = nn, lambda = pi, eps = 1L))
+
+    ### eps is negative
+    expect_error(.ccdr.singleR(cors = cors.test, pp = pp, nn = nn, lambda = pi, eps = -5))
+
+    ### Output warning if user sets eps = 0
+    expect_warning(.ccdr.singleR(cors = cors.test, pp = pp, nn = nn, lambda = pi, eps = 0))
 })
-test_that("Check input: ", {
 
+test_that("Check input: maxIters", {
+    ### maxIters is not an integer
+    expect_error(.ccdr.singleR(cors = cors.test, pp = pp, nn = nn, lambda = pi, maxIters = pi))
+
+    ### maxIters is negative
+    expect_error(.ccdr.singleR(cors = cors.test, pp = pp, nn = nn, lambda = pi, maxIters = -5))
 })
-test_that("Check input: ", {
 
+test_that("Check input: alpha", {
+    ### alpha is negative
+    expect_error(.ccdr.singleR(cors = cors.test, pp = pp, nn = nn, lambda = pi, alpha = -5))
 })
-test_that("Check input: ", {
 
+test_that("Check input: verbose", {
+    ### Works when verbose = TRUE
+    expect_error(.ccdr.singleR(cors = cors.test, pp = pp, nn = nn, lambda = pi, verbose = TRUE))
+
+    ### Works when verbose = FALSE
+    expect_error(.ccdr.singleR(cors = cors.test, pp = pp, nn = nn, lambda = pi, verbose = FALSE))
 })
