2b99d654261ff8d1205b0f834d241ea5c5b29772
itsrainingdata <Zigmund-2@wifi-131-179-20-135.host.ucla.edu>
2015-03-02 14:09:46 -0800

    Renamed methods and refactored code

--- R/ccdr-functions.R
+++ R/ccdr-functions.R
@@ -13,14 +13,38 @@
 #     gen.lambdas
 #
 
-# default / usual function for creating a grid of lambdas
-gen.lambdas <- function(nn,
-                        rlam = 0.001,
-                        nlam = 50
+#' generate.lambdas
+#'
+#' Default / usual function for creating a grid of lambdas
+#' @export
+generate.lambdas <- function(nn,
+                             lambdas.ratio = 0.001,
+                             lambdas.length = 50,
+                             scale = "linear"
+){
+    .gen_lambdas(nn = nn,
+                 lambdas.ratio = lambdas.ratio,
+                 lambdas.length = lambdas.length,
+                 scale = scale)
+} # END .GEN.LAMBDAS
+
+# .gen_lambdas
+#  Internal implementation of generate.lambdas
+.gen_lambdas <- function(nn,
+                         lambdas.ratio = 0.001,
+                         lambdas.length = 50,
+                         scale = "linear"
 ){
     max.lam <- max(sqrt(nn))
-    min.lam <- rlam*max.lam
-    lambda <- exp(seq(log(max.lam), log(min.lam), log(rlam)/(nlam-1)))
+    min.lam <- lambdas.ratio * max.lam
 
-    return(lambda)
-} # END GEN.LAMBDAS
+    if(scale == "linear"){
+        lambdas <- seq(max.lam, min.lam, length.out = lambdas.length)
+    } else if(scale == "log"){
+        lambdas <- exp(seq(log(max.lam), log(min.lam), log(lambdas.ratio)/(lambdas.length-1)))
+    } else{
+        stop("Invalid input for scale argument! Must be either 'log' or 'linear'.")
+    }
+
+    lambdas
+} # END .GEN.LAMBDAS
