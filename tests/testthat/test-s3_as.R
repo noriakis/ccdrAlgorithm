aa89c50981ec711880545fe2fb966b0bc5f8bdaf
itsrainingdata <Zigmund-2@wifi-131-179-20-135.host.ucla.edu>
2015-03-02 14:46:07 -0800

    Updated and added new tests

--- tests/testthat/test-s3_as.R
+++ tests/testthat/test-s3_as.R
@@ -1,9 +1,10 @@
-context("sparse to matrix conversion")
+context("sparse / SparseBlockMatrixR to matrix conversion")
 
 #
 # NOTE: Need to use is_equivalent_to in order to ignore attributes!
 #
 
+### matrix <-> sparse
 test_that("as.matrix -> as.sparse -> as.matrix makes no changes", {
 
     ### Test the zero matrix
@@ -25,19 +26,78 @@ test_that("as.matrix -> as.sparse -> as.matrix makes no changes", {
     expect_that(as.matrix(as.sparse(as.matrix(m))), is_equivalent_to(m))
 })
 
-test_that("as.matrix -> as.sparse -> as.matrix makes no changes to REALLY BIG matrices", {
+### matrix <-> SparseBlockMatrixR
+test_that("as.matrix -> as.SparseBlockMatrixR -> as.matrix makes no changes", {
 
     ### Test the zero matrix
-    m <- matrix(rep(0, 100*100), ncol = 100)
-    expect_that(as.matrix(as.sparse(as.matrix(m))), is_equivalent_to(m))
+    m <- matrix(rep(0, 1), ncol = 1)
+    expect_that(as.matrix(as.SparseBlockMatrixR(as.matrix(m))), is_equivalent_to(m))
+    m <- matrix(rep(0, 4), ncol = 2)
+    expect_that(as.matrix(as.SparseBlockMatrixR(as.matrix(m))), is_equivalent_to(m))
 
-    ### Test a randomly generated matrix
-    m <- random.sparse(100, 5*100)
-    expect_that(as.matrix(as.sparse(as.matrix(m))), is_equivalent_to(m))
+    ### NOTE: Cannot test on random sparse matrix since SBM class ASSUMES a block structure,
+    ###        i.e. induced by a DAG
 
     ### Test a randomly generated DAG
-    m <- random.dag.matrix(100, 5*100)
-    expect_that(as.matrix(as.sparse(as.matrix(m))), is_equivalent_to(m))
+    m <- random.dag.matrix(10, 10)
+    expect_that(as.matrix(as.SparseBlockMatrixR(as.matrix(m))), is_equivalent_to(m))
 })
 
-test_that("as.sparse converts matrix objects correctly", {})
+### sparse <-> SparseBlockMatrixR
+test_that("as.sparse -> as.SparseBlockMatrixR -> as.sparse makes no changes", {
+
+    ### Test the zero matrix
+    m <- matrix(rep(0, 1), ncol = 1)
+    sp <- as.sparse(m)
+    expect_that(as.sparse(as.SparseBlockMatrixR(sp)), equals(sp))
+
+    m <- matrix(rep(0, 4), ncol = 2)
+    sp <- as.sparse(m)
+    expect_that(as.sparse(as.SparseBlockMatrixR(sp)), equals(sp))
+
+    ### NOTE: Cannot test on random sparse matrix since SBM class ASSUMES a block structure,
+    ###        i.e. induced by a DAG
+
+    ### Test a randomly generated DAG
+    m <- random.dag.matrix(10, 10)
+    sp <- as.sparse(m)
+    expect_that(as.sparse(as.SparseBlockMatrixR(sp)), equals(sp))
+})
+
+### SparseBlockMatrixR <-> sparse
+test_that("as.SparseBlockMatrixR -> as.sparse -> as.SparseBlockMatrixR makes no changes", {
+
+    ### Test the zero matrix
+    m <- matrix(rep(0, 1), ncol = 1)
+    sbm <- as.SparseBlockMatrixR(m)
+    expect_that(as.SparseBlockMatrixR(as.sparse(sbm)), equals(sbm))
+
+    m <- matrix(rep(0, 4), ncol = 2)
+    sbm <- as.SparseBlockMatrixR(m)
+    expect_that(as.SparseBlockMatrixR(as.sparse(sbm)), equals(sbm))
+
+    ### NOTE: Cannot test on random sparse matrix since SBM class ASSUMES a block structure,
+    ###        i.e. induced by a DAG
+
+    ### Test a randomly generated DAG
+    m <- random.dag.matrix(10, 10)
+    sbm <- as.SparseBlockMatrixR(m)
+    expect_that(as.SparseBlockMatrixR(as.sparse(sbm)), equals(sbm))
+})
+
+# test_that("as.matrix -> as.sparse -> as.matrix makes no changes to REALLY BIG matrices", {
+#
+#     ### Test the zero matrix
+#     m <- matrix(rep(0, 100*100), ncol = 100)
+#     expect_that(as.matrix(as.sparse(as.matrix(m))), is_equivalent_to(m))
+#
+#     ### Test a randomly generated matrix
+#     m <- random.sparse(100, 5*100)
+#     expect_that(as.matrix(as.sparse(as.matrix(m))), is_equivalent_to(m))
+#
+#     ### Test a randomly generated DAG
+#     m <- random.dag.matrix(100, 5*100)
+#     expect_that(as.matrix(as.sparse(as.matrix(m))), is_equivalent_to(m))
+# })
+#
+# test_that("as.sparse converts matrix objects correctly", {})
--- tests/testthat/test-s3_is.R
+++ tests/testthat/test-s3_is.R
@@ -1,5 +1,6 @@
-context("is_sparse")
+context("is.___ s3 methods")
 
+### S3 sparse
 test_that("is.sparse works as expected", {
     li <- list(rows = integer(0), cols = integer(0), vals = numeric(0), dim = c(1,1), start = 1)
     sp <- sparse(li)
@@ -8,3 +9,17 @@ test_that("is.sparse works as expected", {
     expect_true(is.sparse(sp))
     expect_false(is.sparse(list(0)))
 })
+
+### S3 SparseBlockMatrixR
+test_that("is.SparseBlockMatrixR works as expected", {
+    # "rows", "vals", "blocks", "sigmas", "start"
+    li <- list(rows = integer(0), vals = numeric(0), blocks = integer(0), sigmas = numeric(0), start = 1)
+    sbm <- SparseBlockMatrixR(li)
+    expect_is(sbm, "SparseBlockMatrixR")
+
+    expect_true(is.SparseBlockMatrixR(sbm))
+    expect_false(is.SparseBlockMatrixR(list(0)))
+})
+
+### S3 ccdrFit
+# MISSING!
