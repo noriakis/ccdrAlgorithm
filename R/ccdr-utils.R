7f85fead3ae44fa3719bea8052d2e4eb40f610c8
itsrainingdata <Zigmund-2@MacBook-Pro.local>
2015-05-21 14:26:03 -0700

    Internal methods for -utils.R no longer hidden (tests updated too)

--- R/ccdr-utils.R
+++ R/ccdr-utils.R
@@ -10,25 +10,25 @@
 # PACKAGE CCDR: Utils
 #
 #   CONTENTS:
-#     .check_if_matrix
-#     .list_classes
-#     .check_list_class
-#     .col_classes
-#     .cor_vector
+#     check_if_matrix
+#     list_classes
+#     check_list_class
+#     col_classes
+#     cor_vector
 #
 
 # Special function to check if an object is EITHER matrix or Matrix object
-.check_if_matrix <- function(m){
+check_if_matrix <- function(m){
     is.matrix(m) || inherits(m, "Matrix")
 } # END .CHECK_IF_MATRIX
 
-.check_if_data_matrix <- function(df){
+check_if_data_matrix <- function(df){
     is.data.frame(df) || is.matrix(df)
 } # END .CHECK_IF_DATA_MATRIX
 
 # Count missing values in a matrix or data.frame
-.count_nas <- function(df){
-    if( !.check_if_data_matrix(df)){
+count_nas <- function(df){
+    if( !check_if_data_matrix(df)){
         stop("Input must be a data.frame or a matrix!")
     }
 
@@ -36,12 +36,12 @@
 } # END .COUNT_NAS
 
 # Special function to return types for each element in a list
-.list_classes <- function(li){
+list_classes <- function(li){
     unlist(lapply(li, class))
 } # END .LIST_CLASSES
 
 # Return TRUE if every element of li inherits check.class, FALSE otherwise
-.check_list_class <- function(li, check.class){
+check_list_class <- function(li, check.class){
     if(length(li) == 0){
         warning("List contains no elements!")
 
@@ -52,21 +52,21 @@
 } # END .CHECK_LIST_CLASS
 
 # Output the class of each column in X, return as a character vector
-.col_classes <- function(X){
-    if( !.check_if_data_matrix(X)){
+col_classes <- function(X){
+    if( !check_if_data_matrix(X)){
         stop("Input must be a data.frame or a matrix!")
     }
 
     apply(X, 2, class)
 } # END .COL_CLASSES
 
-.cor_vector <- function(X){
-# This is now implicitly checked via .col_classes
+cor_vector <- function(X){
+# This is now implicitly checked via col_classes
 #     if( !is.data.frame(X) && !is.matrix(X)){
 #         stop("Input must either be a data.frame or a matrix!")
 #     }
 
-    check.numeric <- (.col_classes(X) != "numeric")
+    check.numeric <- (col_classes(X) != "numeric")
     if( any(check.numeric)){
         not.numeric <- which(check.numeric)
         stop(paste0("Input columns must be numeric! Columns ", paste(not.numeric, collapse = ", "), " are non-numeric."))
--- tests/testthat/test-list_classes.R
+++ tests/testthat/test-list_classes.R
@@ -1,13 +1,13 @@
-context(".list_classes")
+context("list_classes")
 
-test_that(".list_classes behaves as expected", {
-    expect_equal(.list_classes(list(numeric(0), integer(0), character(0))),
+test_that("list_classes behaves as expected", {
+    expect_equal(list_classes(list(numeric(0), integer(0), character(0))),
                  c("numeric", "integer", "character"))
 
     ### Use a named list
-    expect_that(.list_classes(list(x = numeric(0), y = integer(0), z = character(0))),
+    expect_that(list_classes(list(x = numeric(0), y = integer(0), z = character(0))),
                 is_equivalent_to(c("numeric", "integer", "character"))) # Need to use is_equivalent_to here to ignore attributes
 
     ### Empty list
-    expect_equal(.list_classes(list()), c())
+    expect_equal(list_classes(list()), c())
 })
