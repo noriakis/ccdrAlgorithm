a2d4a003a8be3fbae8eeac8ffa121fe7e89fb3e1
itsrainingdata <Zigmund-2@MacBook-Pro.local>
2015-07-14 11:12:26 -0700

    Added package startup warning

--- R/zzz.R
+++ R/zzz.R
@@ -5,3 +5,7 @@
 #  Created by Bryon Aragam (local) on 2/4/15.
 #  Copyright (c) 2014-2015 Bryon Aragam (local). All rights reserved.
 #
+
+.onAttach <- function(libname, pkgname){
+    packageStartupMessage("NOTE: This package is currently in a development state and may be unstable.\n Please report any bugs to the author.")
+}
