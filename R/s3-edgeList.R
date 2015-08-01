be6d16bf01180c77717672c90497e4272ca99b66
itsrainingdata <Zigmund-2@MacBook-Pro.lan>
2015-07-31 18:11:07 -0700

    Fixes to edgeList output and docs

--- R/s3-edgeList.R
+++ R/s3-edgeList.R
@@ -25,14 +25,12 @@
 #' corresponds to a node, and each component is an integer vector whose components are the parents
 #' of this node in the graph. Only intended for internal use.
 #'
-#' @section Slots:
-#' \describe{
-#' \item{\code{edges}}{Edge list representation of a graph sorted by children.}
-#' }
+#' Also inherits from \code{\link{list}}.
 #'
 #'
 #' @section Methods:
-#' \code{\link{get.adjacency.matrix}}
+#' \code{\link{get.adjacency.matrix}},
+#' \code{\link{num.nodes}}, \code{\link{num.edges}}
 #'
 #' @docType class
 #' @name edgeList-class
@@ -50,22 +48,26 @@ edgeList.list <- function(li){
         stop("Input must be a list!")
     }
 
-    structure(li, class = "edgeList")
+    structure(li, class = c("edgeList", "list"))
 } # END EDGELIST.LIST
 
 #' @export
 print.edgeList <- function(edgeL){
-    ### Assumes the DAG has at most 1000 nodes: Output will be cramped and illegible if the graph is larger than this
-    ### We shouldn't be printing this when pp > 1000 anyway!
-    edgeL.out <- mapply(function(x, y){
-        prefix <- paste0("[", x, "]")
-        prefix <- sprintf("%-5s", prefix)
-        paste0(prefix, paste(sprintf("%4d", sort(y)), collapse = ""))
-    }, 1L:length(edgeL), edgeL)
-    edgeL.out <- unlist(edgeL.out)
-    edgeL.out <- paste(edgeL.out, collapse = " \n")
+    if(num.edges(edgeL) == 0){
+        edgeL.out <- paste0("<Empty graph on ", num.nodes(edgeL), " nodes.>")
+    } else{
+        ### Assumes the DAG has at most 1000 nodes: Output will be cramped and illegible if the graph is larger than this
+        ### We shouldn't be printing this when pp > 1000 anyway!
+        edgeL.out <- mapply(function(x, y){
+            prefix <- paste0("[", x, "]")
+            prefix <- sprintf("%-5s", prefix)
+            paste0(prefix, paste(sprintf("%4d", sort(y)), collapse = ""))
+        }, 1L:length(edgeL), edgeL)
+        edgeL.out <- unlist(edgeL.out)
+        edgeL.out <- paste(edgeL.out, collapse = " \n")
+    }
 
-    cat(edgeL.out, "\n")
+    cat("edgeList object\n", edgeL.out, "\n", sep = "")
 }
 
 #' @export
