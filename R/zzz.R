066ab3df43d4bd10d9b4da214003f78e912d3333
itsrainingdata <bryonsgit@gmail.com>
2016-03-10 15:06:35 -0800

    Minor tweaks

--- R/zzz.R
+++ R/zzz.R
@@ -6,7 +6,7 @@
 #  Copyright (c) 2014-2016 Bryon Aragam. All rights reserved.
 #
 
-
 .onAttach <- function(libname, pkgname){
-    packageStartupMessage("NOTE: This package is currently in a development state and may be unstable.\n Please report any bugs at https://github.com/itsrainingdata/ccdrAlgorithm/issues.")
+    ### Only sparsebn needs a package startup message
+    # packageStartupMessage("NOTE: This package is currently in a development state and may be unstable.\n Please report any bugs at https://github.com/itsrainingdata/ccdrAlgorithm/issues.")
 }
