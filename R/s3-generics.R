c131a2ff1b6f46a9f8d8a84febef0b3e73a41cd2
itsrainingdata <bryonsgit@gmail.com>
2016-03-10 16:54:14 -0800

    Use @importFrom instead of re-defining generics

--- R/s3-generics.R
+++ R/s3-generics.R
@@ -12,12 +12,37 @@
 #   CONTENTS:
 #
 
-# Generics for SparseBlockMatrixR
+# Generics for SparseBlockMatrixR ------------------------------------------------------------
 SparseBlockMatrixR <- function(x) UseMethod("SparseBlockMatrixR", x)
 as.SparseBlockMatrixR <- function(x) UseMethod("as.SparseBlockMatrixR", x)
 to_B <- function(x) UseMethod("to_B", x)
 
-# Taken from sparsebnUtils
-num.nodes <- function(x) UseMethod("num.nodes", x)
-num.edges <- function(x) UseMethod("num.edges", x)
-is.zero <- function(x) UseMethod("is.zero", x)
+# Taken from sparsebnUtils -------------------------------------------------------------------
+# num.nodes <- function(x) UseMethod("num.nodes", x)
+# num.edges <- function(x) UseMethod("num.edges", x)
+# is.zero <- function(x) UseMethod("is.zero", x)
+
+# Imported from sparsebnUtils ----------------------------------------------------------------
+#' @importFrom sparsebnUtils reIndexC
+#' @export
+sparsebnUtils::reIndexC
+
+#' @importFrom sparsebnUtils reIndexR
+#' @export
+sparsebnUtils::reIndexR
+
+#' @importFrom sparsebnUtils get.adjacency.matrix
+#' @export
+sparsebnUtils::get.adjacency.matrix
+
+#' @importFrom sparsebnUtils num.nodes
+#' @export
+sparsebnUtils::num.nodes
+
+#' @importFrom sparsebnUtils num.edges
+#' @export
+sparsebnUtils::num.edges
+
+#' @importFrom sparsebnUtils is.zero
+#' @export
+sparsebnUtils::is.zero
