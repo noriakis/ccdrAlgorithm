5f63a97b45205ee04ee94adee674278d4d8b92a9
itsrainingdata <bryonsgit@gmail.com>
2016-04-05 23:05:42 -0700

    Fixed WARNING in check

--- R/s3-generics.R
+++ R/s3-generics.R
@@ -24,25 +24,25 @@ to_B <- function(x) UseMethod("to_B", x)
 
 # Imported from sparsebnUtils ----------------------------------------------------------------
 #' @importFrom sparsebnUtils reIndexC
-#' @export
-sparsebnUtils::reIndexC
+# #' @export
+# sparsebnUtils::reIndexC
 
 #' @importFrom sparsebnUtils reIndexR
-#' @export
-sparsebnUtils::reIndexR
+# #' @export
+# sparsebnUtils::reIndexR
 
 #' @importFrom sparsebnUtils get.adjacency.matrix
-#' @export
-sparsebnUtils::get.adjacency.matrix
+# #' @export
+# sparsebnUtils::get.adjacency.matrix
 
 #' @importFrom sparsebnUtils num.nodes
-#' @export
-sparsebnUtils::num.nodes
+# #' @export
+# sparsebnUtils::num.nodes
 
 #' @importFrom sparsebnUtils num.edges
-#' @export
-sparsebnUtils::num.edges
+# #' @export
+# sparsebnUtils::num.edges
 
 #' @importFrom sparsebnUtils is.zero
-#' @export
-sparsebnUtils::is.zero
+# #' @export
+# sparsebnUtils::is.zero
