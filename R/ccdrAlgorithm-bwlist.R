6b499543cc9f79af74367347647abfb3732c1dd7
itsrainingdata <bryonsgit@gmail.com>
2017-08-11 16:02:23 -0400

    Add some internal documentation

--- R/ccdrAlgorithm-bwlist.R
+++ R/ccdrAlgorithm-bwlist.R
@@ -21,11 +21,14 @@ names_to_indices <- function(v, names){
     match(v, names)
 } # END NAMES_TO_INDICES
 
+### Returns a list whose components are the rows of a matrix
 rows_to_list <- function(m){
     lapply(1:nrow(m), function(j) m[j,])
 } # END ROWS_TO_LIST
 
+### Check correctness of input and transform from matrix to list of indices
 bwlist_check <- function(bwlist, names){
+    ## Consistency checks
     if(!is.matrix(bwlist) || ncol(bwlist) != 2){
         stop("Input must be a matrix with exactly 2 columns!")
     }
@@ -34,16 +37,21 @@ bwlist_check <- function(bwlist, names){
         stop("Input cannot have missing values!")
     }
 
+    ### Convert characters names to indices
     if(is.character(bwlist)){
         bwlist <- as.vector(bwlist)
         bwlist <- names_to_indices(bwlist, names)
         bwlist <- matrix(bwlist, ncol = 2)
     }
 
-    storage.mode(bwlist) <- "integer"
+    storage.mode(bwlist) <- "integer" # This is important in ccdr_call to check overlap between blacklist and whitelist, fails if numerics are mixed with ints
     rows_to_list(bwlist)
 } # END BWLIST_CHECK
 
+### Convert b/w lists to weight matrix of {-1,0,1}
+#     -1 = black listed (guaranteed to be absent / zero)
+#      0 = white listed (guaranteed to be present / nonzero)
+#      1 = gray listed (may or may not be final model)
 bwlist_to_weights <- function(black, white, nnode){
     weights <- matrix(1L, ncol = nnode, nrow = nnode)
 
